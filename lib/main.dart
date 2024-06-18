9a3fa461a536a35fb9062303c728d63a695f8a28
Marty Fuhry <martyfuhry@gmail.com>
2024-06-18 11:09:26 -0400

    Tested!

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:background_downloader/background_downloader.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:workmanager/workmanager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FileDownloader().trackTasks();
   Workmanager().initialize(
       callbackDispatcher, // The top level function, aka callbackDispatcher
       isInDebugMode:
           true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
       );
-  Workmanager().registerOneOffTask("task-identifier", "simpleTask");
   runApp(const MyApp());
 }
 
@@ -63,32 +69,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ValueNotifier<List<String>> _files = ValueNotifier([]);
+  final ValueNotifier<List<TaskRecord>> _tasks = ValueNotifier([]);
+  StreamSubscription? _sub;
+  Timer? _timer;
+  StreamSubscription? _downloaderStatus;
+
+  @override
+  void initState() {
+    unawaited(_readTasks());
+    _downloaderStatus = FileDownloader().updates.listen((status) async {
+      await _readTasks();
+    });
+
+    // Poll the tasks
+    /*
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _readTasks();
+    });
+    */
+    super.initState();
+  }
+
+  Future<void> _readFiles() async {
+    final directory = await getApplicationDocumentsDirectory();
+    _files.value = directory.listSync().map((f) => _filename(f)).toList();
+
+    _sub = directory.watch().listen((events) {
+      _files.value = directory.listSync().map((f) => _filename(f)).toList();
+    });
+  }
+
+  Future<void> _readTasks() async {
+    final tasks = await FileDownloader().database.allRecords();
+    _tasks.value = tasks;
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _sub?.cancel();
+    _downloaderStatus?.cancel();
+    super.dispose();
+  }
+
+  String _filename(FileSystemEntity f) {
+    return f.path.split('/').last;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Downloadatron'),
+        title: const Text('Files in Application Directory'),
       ),
-      body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Background tests',
-            ),
-          ],
+      body: ValueListenableBuilder(
+        valueListenable: _tasks,
+        builder: (context, tasks, child) => ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(tasks[index].taskId),
+            subtitle: Text(tasks[index].status.toString()),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: () async {
+          await _readTasks();
+          Workmanager().registerOneOffTask(
+            "task-identifier",
+            "simpleTask",
+            initialDelay: const Duration(seconds: 5),
+          );
+          await _readTasks();
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -96,25 +150,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+DownloadTask _createDownloadTask({String type = 'background'}) {
+  return DownloadTask(
+    url: 'https://google.com/search',
+    urlQueryParameters: {'q': 'pizza'},
+    filename: '$type-file-${Random().nextInt(100000)}.html',
+    metaData: type,
+  );
+}
+
 @pragma(
     'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    final updates = FileDownloader().updates.listen((update) {
-      print('update status is $update');
-    });
-    print(
-        "Native called background task: $task"); //simpleTask will be emitted here.
-    final download = DownloadTask(
-      url: 'www.google.com',
-      filename: 'file',
-      updates: Updates.statusAndProgress,
-    );
-    final success = await FileDownloader().enqueue(
-      download,
-    );
-    print('Successfully enqueued a task $success');
-    updates.cancel();
-    return Future.value(true);
+    //  start tracking tasks in the database
+    await FileDownloader().trackTasks();
+    final download = _createDownloadTask();
+    print('background download for $download');
+    final success = await FileDownloader().enqueue(download);
+    return Future.value(success);
   });
 }
